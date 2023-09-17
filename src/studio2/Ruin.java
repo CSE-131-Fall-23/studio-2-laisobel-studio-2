178393dd1bfb87260a88e94f039291ab09991bbb
enochlai <enochlai@10.232.134.164>
2023-09-17 14:41:03 -0500

    LOL

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -6,39 +6,52 @@ public class Ruin {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		Scanner in = new Scanner(System.in);
 		System.out.println("Start Amount: ");
 		double startAmount = in.nextDouble();
+		
 		System.out.println("Win Chance (between 0 & 1): ");
 		double winChance = in.nextDouble();
-		System.out.println("Win Limit: 23r2w ");
+		
+		System.out.println("Win Limit: ");
 		double winLimit = in.nextDouble(); 
+		
+		int totalDays = 0;
 		int totalSimulations = 0;
-		
-		for (int days = 0; days < 500; days++)
-			{double newstartAmount = startAmount;
-			while ((startAmount < winLimit) && (startAmount > 0))
-				
+		while (totalDays < 500)
+		{
+		double newStartAmount = startAmount;
+		while ((newStartAmount < winLimit) && (newStartAmount > 0))
 			{
-			
-			if (Math.random() > (1.00 - winChance))
-			{newstartAmount++;
+				if (Math.random() > (1.00 - winChance))
+					{
+					newStartAmount++;
+					totalSimulations++;
+					}
+					
+				else
+					{
+					newStartAmount--;
+					totalSimulations++;
+					}
 			}
-			
-			else
-			{newstartAmount--;
+		
+		totalDays++;
+		
+		if (newStartAmount == 0)
+			{
+			System.out.println("ON DAY " + totalDays + " YOU LOSE AFTER PLAYING " + totalSimulations + "TIMES");
+			totalSimulations = 0;
+			}
+		else 
+			{
+			System.out.println("ON DAY " + totalDays + " YOU WIN AFTER PLAYING " + totalSimulations + " TIMES");
+			totalSimulations = 0;
 			}
 		
-			totalSimulatio3ns++;
-			System.out.println("You played uhiuhiuh" + totalSimulations + " Times");
 		
-		startAmount = startAmount;
-			startAmount--;
-			
-			
 		}
-		}
-		
 	}
-
 }
+				
